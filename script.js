c7b1d80a19d3f3a57caed45c7e8476c4466bc147
evanburton77 <evanburton77@gmail.com>
2021-04-09 19:52:07 -0500

    friday

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const button = document.querySelector(".randomNHL")
+const nhlHeading = document.querySelector("#my-NHL-header")
+const textInput = document.querySelector("#textInput")
 
+async function getTeam(event){
+    event.preventDefault()
+    fetch(`https://statsapi.web.nhl.com/api/v1/teams/${textInput.value}`)
+    .then(res => {
+        return res.json()
+      })
+    .then(res => 
+        nhlHeading.innerText = `NHL Venue: ${res.teams[0].venue.name}`)
+        
+}
+
+button.addEventListener("click", getTeam)
