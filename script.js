8c5c27f865a1deb1b75df0de5cc656f4523a479c
evanburton77 <evanburton77@gmail.com>
2021-04-11 14:19:51 -0500

    Q&A

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".randomNHL")
 const nhlHeading = document.querySelector("#my-NHL-header")
 const textInput = document.querySelector("#textInput")
+const textInput2 = document.querySelector("#textInput2")
+const button2 = document.querySelector(".answer")
 
 async function getTeam(event){
     event.preventDefault()
@@ -8,9 +10,15 @@ async function getTeam(event){
     .then(res => {
         return res.json()
       })
-    .then(res => 
-        nhlHeading.innerText = `NHL Venue: ${res.teams[0].venue.name}`)
+    .then(res => {
+        nhlHeading.innerText = `NHL Team Name: ${res.teams[0].teamName}`
         
+    
+        if(textInput2.value === res.teams[0].locationName){
+           alert("CORRECT!")
+        }})
 }
 
 button.addEventListener("click", getTeam)
+button2.addEventListener("click", getTeam)
+
